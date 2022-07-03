d040fe300302e4f6e0115b30b72fbe8967fbc4ec
hp <hp@DESKTOP-SEHLU5V>
2022-07-03 10:28:35 +0530

    one more commit...

--- src/com/dss/Demo1.java
+++ src/com/dss/Demo1.java
@@ -1,13 +1,19 @@
 package com.dss;
 
 public class Demo1 {
+	
+	
+	public void m1(int x)
+	{
+		System.out.println(x*x);
+	}
 
 	
 	public static void main(String[] args) {
 		
+		Demo1 demo1=new Demo1();
 		
-		System.out.println("this is sample method.....");
-		System.out.println("this is one java class.....");
+		demo1.m1(50);
 		
 	}
 
